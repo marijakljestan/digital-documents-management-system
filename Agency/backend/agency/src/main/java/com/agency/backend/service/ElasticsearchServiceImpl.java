5cb41be68fcd94d1a98f74ee0981593731119afe
Marija Klještan <marijakljestan@uns.ac.rs>
2023-02-16 21:17:36 +0100

    feat: generate highlight

--- Agency/backend/agency/src/main/java/com/agency/backend/service/ElasticsearchServiceImpl.java
+++ Agency/backend/agency/src/main/java/com/agency/backend/service/ElasticsearchServiceImpl.java
@@ -2,6 +2,7 @@ package com.agency.backend.service;
 
 import com.agency.backend.dto.SearchResult;
 import com.agency.backend.dto.SimpleQueryDto;
+import com.agency.backend.mapper.SearchResultMapper;
 import com.agency.backend.model.CandidateIndexUnit;
 import com.agency.backend.service.interfaces.ElasticsearchService;
 import lombok.AllArgsConstructor;
@@ -18,6 +19,7 @@ import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.sql.SQLSyntaxErrorException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -43,8 +45,7 @@ public class ElasticsearchServiceImpl implements ElasticsearchService {
         }
 
         String[] arr = new String[fields.size()];
-        arr = fields.toArray(arr);
-        QueryBuilder searchQuery = buildSearchQuery(query.toString().trim(), arr);
+        BoolQueryBuilder searchQuery = buildSearchQuery(query.toString().trim(), fields.toArray(arr));
         return executeSearch(searchQuery);
     }
 
@@ -66,7 +67,7 @@ public class ElasticsearchServiceImpl implements ElasticsearchService {
         return executeSearch(queryBuilder);
     }
 
-    private QueryBuilder buildSearchQuery(String query, String[] fields) {
+    private BoolQueryBuilder buildSearchQuery(String query, String[] fields) {
         if (query == null || fields.length < 1)  return null;
 
         BoolQueryBuilder queryBuilder = new BoolQueryBuilder();
@@ -77,42 +78,52 @@ public class ElasticsearchServiceImpl implements ElasticsearchService {
     }
 
     private List<SearchResult> executeSearch(QueryBuilder searchQuery) {
+        SearchRequest searchRequest = buildSearchRequest(searchQuery);
+        List<SearchResult> results = new ArrayList<>();
+        try {
+            SearchResponse searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+            results = processSearchHits(searchResponse.getHits().getHits());
+        } catch (IOException e) {
+            LOGGER_ERROR.error("ES SERVICE: executeSearch - ", e);
+        }
+        return results;
+    }
 
-//        HighlightBuilder highlightBuilder = new HighlightBuilder();
-//        HighlightBuilder.Field cvContentHighlight = new HighlightBuilder.Field("cvContent");
-//        highlightBuilder.field(cvContentHighlight);
-//        highlightBuilder.preTags("<em>");
-//        highlightBuilder.postTags("</em>");
-//        highlightBuilder.fragmentSize(150);
+    private  List<SearchResult> processSearchHits(SearchHit[] searchHits) {
+        List<SearchResult> results = new ArrayList<>();
+        for (SearchHit hit : searchHits) {
+            SearchResult result = SearchResultMapper.toSearchResult(hit);
+            result.setHighlight(getHighlightContent(hit));
+            results.add(result);
+        }
+        return results;
+    }
+
+    private String getHighlightContent(SearchHit hit) {
+        Map<String, HighlightField> highlightFields = hit.getHighlightFields();
+        if(highlightFields.isEmpty()) return "";
+
+        HighlightField highlightedField = highlightFields.get("cvContent");
+        return highlightedField.fragments()[0].string();
+    }
+
+    private SearchRequest buildSearchRequest(QueryBuilder searchQuery) {
+        HighlightBuilder highlightBuilder = getHighlightBuilder();
 
         SearchRequest searchRequest = new SearchRequest(candidateIndex);
         SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
         sourceBuilder.query(searchQuery);
+        sourceBuilder.highlighter(highlightBuilder);
         searchRequest.source(sourceBuilder);
+        return searchRequest;
+    }
 
-        List<SearchResult> results = new ArrayList<>();
-        try {
-            SearchResponse searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
-            SearchHit[] searchHits = searchResponse.getHits().getHits();
-
-            for (SearchHit hit : searchHits) {
-                Map<String, Object> sourceAsMap = hit.getSourceAsMap();
-                HighlightField cvContentHighlightField = hit.getHighlightFields().get("cvContent");
-                results.add(SearchResult.builder()
-                        .firstName(sourceAsMap.get("firstName").toString())
-                        .lastName(sourceAsMap.get("lastName").toString())
-                        .degree(sourceAsMap.get("degree").toString())
-                        .cvContent(sourceAsMap.get("cvContent").toString())
-                        .coverLetterContent(sourceAsMap.get("coverLetterContent").toString())
-                        //.highlight(cvContentHighlightField.fragments()[0].string())
-                                .highlight("")
-                        .build());
-            }
-        } catch (IOException e) {
-            LOGGER_ERROR.error("ES SERVICE: executeSearch - ", e);
-            e.printStackTrace();
-        }
-
-        return results;
+    private static HighlightBuilder getHighlightBuilder() {
+        HighlightBuilder highlightBuilder = new HighlightBuilder();
+        highlightBuilder.field("cvContent");
+        highlightBuilder.preTags("<em>");
+        highlightBuilder.postTags("</em>");
+        highlightBuilder.fragmentSize(150);
+        return highlightBuilder;
     }
 }
